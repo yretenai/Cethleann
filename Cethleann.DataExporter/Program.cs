334ff6e606a7147170d9c702fd1df064bf8157d6
Naomi <614231+healingbrew@users.noreply.github.com>
2019-09-09 00:39:32 +0100

    Add DATA0/DATA1 decompress/extraction

---
+++ Cethleann.DataExporter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Cethleann.DataExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var romfs = args.Last();
+            var DATA0 = new DATA0(@$"{romfs}\DATA0.bin");
+            using var DATA1 = File.OpenRead(@$"{romfs}\DATA1.bin");
+            var i = 0;
+
+            if (!Directory.Exists(@$"{romfs}\ex\uncompressed")) Directory.CreateDirectory(@$"{romfs}\ex\uncompressed");
+            if (!Directory.Exists(@$"{romfs}\ex\compressed")) Directory.CreateDirectory(@$"{romfs}\ex\compressed");
+
+            foreach (var entry in DATA0.Entries)
+            {
+                using var data = DATA0.ReadEntry(DATA1, entry);
+                File.WriteAllBytes(@$"{romfs}\ex\{(entry.IsCompressed ? "" : "un")}compressed\{i++:X16}.bin", data.ToArray());
+                Console.WriteLine(@$"{romfs}\ex\{(entry.IsCompressed ? "" : "un")}compressed\{i:X16}.bin");
+            }
+        }
+    }
+}
---
+++ Cethleann/Structure/DATA1CompressionInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Cethleann.Structure
+{
+    /// <summary>
+    /// Info regarding compression for a file in DATA1
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 4, Size = 12)]
+    public struct DATA1CompressionInfo
+    {
+        /// <summary>
+        /// Always 0x10000, I assumed ChunkSize but it doesn't match up.
+        /// </summary>
+        public int Unknown { get; set; }
+        /// <summary>
+        /// Number of chunks
+        /// </summary>
+        public int ChunkCount { get; set; }
+        /// <summary>
+        /// Compressed (read: bytes to read in this file) size of this file.
+        /// Don't know why this exist, maybe anonymous file pointers?
+        /// </summary>
+        public int Size { get; set; }
+    }
+}
