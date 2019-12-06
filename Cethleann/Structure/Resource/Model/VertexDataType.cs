f703b8042d8f76dc8a464b51455d1acec3389407
Naomi Ahmed <614231+healingbrew@users.noreply.github.com>
2019-12-06 17:19:47 +0000

    [skip ci]

--- Cethleann/Structure/Resource/Model/VertexDataType.cs
+++ Cethleann/Structure/Resource/Model/VertexDataType.cs
@@ -7,6 +7,7 @@ namespace Cethleann.Structure.Resource.Model
         Vector3Single = 0x0002,
         Vector4Single = 0x0003,
         Vector4Byte = 0x0005,
+        Vector4HalfNormalized = 0x0007,
         Vector2Half = 0x000A,
         Vector4Half = 0x000B,
         Vector4ByteNormalized = 0x000D
