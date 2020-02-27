cc559c4d6c028a8eec69c020b998a15ec9dfceda
Naomi <614231+healingbrew@users.noreply.github.com>
2020-02-27 07:57:24 +0000

    [Nyotengu.Database] remove unused using directive.
    [note] 6bb23e3 actually references Nyotengu.Database

--- Nyotengu.KTID/Program.cs
+++ Nyotengu.KTID/Program.cs
@@ -3,7 +3,6 @@ using System.Buffers.Binary;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Runtime.InteropServices;
 using Cethleann.Graphics;
 using Cethleann.KTID;
