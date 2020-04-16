a4e748fbf733739d7281fecd63073524c2d38c2f
Naomi <614231+healingbrew@users.noreply.github.com>
2020-04-16 01:38:34 +0100

    [Nyotengu] add Nyotengu.Fileist
    [Filelist] update DoA6 list

---
+++ Nyotengu.Filelist/FilelistFlags.cs
@@ -0,0 +1,22 @@
+﻿using Cethleann.ManagedFS.Options;
+using Cethleann.Structure;
+using DragonLib.CLI;
+using JetBrains.Annotations;
+
+namespace Nyotengu.Filelist
+{
+    [UsedImplicitly]
+    public class FilelistFlags : ICLIFlags, INyotenguOptions
+    {
+        [CLIFlag("game-dir", Positional = 0, Help = "Game Directorie", IsRequired = true, Category = "Filelist Options")]
+        public string? GameDir { get; set; }
+
+        [CLIFlag("filelist", Help = "File List to load. Unspecified is automatically determined based on GameId", Category = "Database Options")]
+        public string? FileList { get; set; }
+
+        [CLIFlag("game", Default = DataGame.None, Aliases = new[] { "g" }, Help = "Game being loaded", Category = "Database Options")]
+        public DataGame GameId { get; set; }
+
+        public bool NyotenguPrefixFilenames { get; set; }
+    }
+}
---
+++ Nyotengu.Filelist/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using DragonLib.CLI;
+using DragonLib.IO;
+
+namespace Nyotengu.Filelist
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Logger.PrintVersion("Nyotengu");
+            var flags = CommandLineFlags.ParseFlags<FilelistFlags>(CommandLineFlags.PrintHelp, args);
+            if (flags == null || string.IsNullOrWhiteSpace(flags.GameDir)) return;
+
+            var nyotengu = new Cethleann.ManagedFS.Nyotengu(flags);
+            nyotengu.LoadFileList(flags.FileList, flags.GameId);
+            foreach (var rdb in Directory.GetFiles(flags.GameDir, "*.rdb")) nyotengu.AddDataFS(rdb);
+            nyotengu.SaveGeneratedFileList(null, flags.GameId);
+        }
+    }
+}
